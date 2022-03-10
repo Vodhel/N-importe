a9d28fd57cbef7aaa2c20221e5cfe34852910930
Clai-re <claire2001wu@gmail.com>
2022-03-10 20:26:53 +0100

    jeudi

--- main.c
+++ main.c
@@ -14,25 +14,25 @@ void checkXML(char* filename){
   char* tag;
   Pile p=NULL;
 
-  while((s=nextString(xmlFile))!=NULL){
+  while((s=nextString(xmlFile))!=NULL){   
 
     if(isTag(s)){
       if(isOpeningTag(s)){
         tag=malloc(sizeof(s));
         strcpy(tag,s);
-        empile(&p,tag);
+        empile(&p,tag);         //on empile seulement les balises ouvrantes
       }
 
       if(isClosingTag(s)){
         if(estPileVide(p)){
-          printf("Pas de balise ouvrante pour %s\n",s);
+          printf("Pas de balise ouvrante pour %s\n",s);   //Si on rencontre une balise fermante et que la pile est vide alors il manque une balise ouvrante
           closeXML(xmlFile);
           return;
 
         }else{
           char *temp=depile(&p);
 
-          if(strcmp(tagName(s),tagName(temp))){
+          if(strcmp(tagName(s),tagName(temp))){   //Sinon on verifie que la balise fermante correspond bien à la balise ouvrante en tete de pile
             printf("balise %s lu, mais </%s> attendu\n", s, tagName(temp));
             free(tag);
             free(temp);
@@ -45,10 +45,10 @@ void checkXML(char* filename){
     }
   }
 
-  if(!estPileVide(p)){
+  if(!estPileVide(p)){  //Si apres le parcours du fichiers xml, dans la pile il reste des balises ouvrantes alors il manque des balises fermantes
     printf("Pas de balise fermante pour %s\n", tetePile(p));
   }else{
-    printf("Pas d'erreur\n");
+    printf("Pas d'erreur\n"); //Sinon aucune erreur
   }
 
   free(tag);
@@ -74,21 +74,23 @@ void drawGraph(char* filename,FILE* out) {
 
       if(isOpeningTag(s)){
 
-        noeud++;
+        noeud++; //donne le id du noeud correspondant
 
-        fprintf(out,"\t %d [label=\"%s\"];\n", noeud, tagName(s));
+        fprintf(out,"\t %d [label=\"%s\"];\n", noeud, tagName(s)); //print la declaration d'un sommet
 
         if(!estPileVide(p)){
-          fprintf(out,"\t %d -> %d\n", tetePile2(p), noeud);
+          fprintf(out,"\t %d -> %d\n", tetePile2(p), noeud);  //si la pile n'est pas vide alors le noeud à un père
+                                                              //on print alors l'arc correspondant
         }
 
         tag=malloc(sizeof(s));
         strcpy(tag,s);
-        empile2(&p,tag,noeud);
+        empile2(&p,tag,noeud);  //on empile tous les balises ouvrantes et leurs id
+                                //empile2 ajoute en tete de pile la balise et le id du noeud
 
       }
 
-      if(isClosingTag(s)){
+      if(isClosingTag(s)){  //on depile tous les balises qui ont ete refermées
         depile(&p);
       }
     }
